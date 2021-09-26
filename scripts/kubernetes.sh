f8a17a541e9072575d3273df130a991147243af0
Burak DAĞDEVİREN <tresofya@hotmail.com>
2021-09-26 16:31:50 +0300

    flatcar

---
+++ scripts/kubernetes.sh
@@ -0,0 +1,21 @@
+CNI_VERSION="v0.8.2"
+CRICTL_VERSION="v1.17.0"
+RELEASE_VERSION="v0.4.0"
+DOWNLOAD_DIR=/opt/bin
+RELEASE="v1.22.2"
+
+mkdir -p /opt/cni/bin
+mkdir -p /etc/systemd/system/kubelet.service.d
+
+alias curl='curl -sSL'
+curl "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+curl "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C $DOWNLOAD_DIR -xz
+curl "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service
+curl "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+curl --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
+
+chmod +x {kubeadm,kubelet,kubectl}
+mv {kubeadm,kubelet,kubectl} $DOWNLOAD_DIR/
+
+systemctl enable --now kubelet
+systemctl status kubelet
---
+++ scripts/network.sh
@@ -0,0 +1,12 @@
+systemctl enable docker
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
